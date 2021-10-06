77223adb731f59274485b69d85a6ef29b92ccde8
reinesmaliksi <rmaliksi@umd.edu>
2021-10-06 16:00:50 -0400

    update 8

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -8,7 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const startBtn = document.querySelector('#start-button');
   const width = 10;
   let nextRandom = 0;
-  let timerId
+  let timerId;
+  let score = 0;
+  const colors = [
+      'orange',
+      'red',
+      'purple',
+      'green',
+      'blue'
+  ]
 
   // The Tetrominoes
   const lTetromino = [
@@ -59,6 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function draw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.add('tetromino');
+      squares[currentPosition + index].style.backgroundColor = colors[random];
     });
   }
 
@@ -66,11 +75,12 @@ document.addEventListener('DOMContentLoaded', () => {
   function undraw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.remove('tetromino');
+      squares[currentPosition + index].style.backgroundColor = ''
     });
   }
 
   // make the tetromino move down every second
-  timerId = setInterval(moveDown, 1000);
+  // timerId = setInterval(moveDown, 1000);
 
   // assign functions to keyCodes
   function control(e) {
@@ -105,6 +115,8 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore();
+      gameOver();
     }
   }
 
@@ -166,17 +178,54 @@ document.addEventListener('DOMContentLoaded', () => {
       // remove any trace of a tetromino from the entire grid
       displaySquares.forEach(square => {
           square.classList.remove('tetromino')
+          square.style.backgroundColor = ''
       })
       upNextTetrominoes[nextRandom].forEach(index => {
           displaySquares[displayIndex + index].classList.add('tetromino')
+          displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
       })
   }
 
   // add functionality to the button
   startBtn.addEventListener('click', () => {
       if (timerId) {
-          clearInterval(timerId)
+          clearInterval(timerId);
+          timerId = null;
+      } else {
+          draw();
+          timerId = setInterval(movdDown, 1000);
+          nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+          displayShape();
       }
   })
+
+  //add score
+  function addScore() {
+      for (let i = 0; i < 199; i += width) {
+          const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+          if (row.every(index => squares[index].classList.contains('taken'))) {
+              score += 10
+              scoreDisplay.innerHTML = score
+              row.forEach(index => {
+                  squares[index].classList.remove('taken')
+                  squares[index].classList.remove('tetromino')
+                  squares[index].style.backgroundColor = ''
+              })
+              const squaresRemoved = squares.splice(i, width)
+              squares = squaresRemoved.concat(squares)
+              squares.forEach(cell => grid.appendChild(cell))
+          }
+      }
+  }
+
+  //game over
+  function gameOver() {
+      if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+          scoreDisplay.innerHTML = 'end'
+          clearInterval(timerId)
+
+      }
+  }
 
 });
