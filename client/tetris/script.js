01a6702ac6452f4b2eb21b811c2c607d98cc1f15
reinesmaliksi <rmaliksi@umd.edu>
2021-10-06 14:14:16 -0400

    update 6

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-use-before-define */
+const { noExtendLeft } = require("sequelize/types/lib/operators");
+
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
-  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const squares = Array.from(document.querySelectorAll('.grid div'));
   const ScoreDisplay = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
@@ -44,28 +47,42 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
   let currentPosition = 4;
-  let currentRotation = 0;
+  const currentRotation = 0;
 
   // randomly let a tetronimo and its first rotation
-  let random = Math.floor(Math.random()*theTetrominoes.length);
+  let random = Math.floor(Math.random() * theTetrominoes.length);
   let current = theTetrominoes[random][currentRotation];
 
   // draw the Tetromino
   function draw() {
-    current.forEach(index => {
+    current.forEach((index) => {
       squares[currentPosition + index].classList.add('tetromino');
     });
   }
 
   // undraw the Tetromino
   function undraw() {
-    current.forEach(index => {
+    current.forEach((index) => {
       squares[currentPosition + index].classList.remove('tetromino');
     });
   }
 
   // make the tetromino move down every second
   timerId = setInterval(moveDown, 1000);
+
+  // assign functions to keyCodes
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+  document.addEventListener('keyup', control);
 
   // move down function
   function moveDown() {
@@ -77,8 +94,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // freeze function
   function freeze() {
-    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+    if (current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
       // start a new tetromino falling
       random = Math.floor(Math.random() * theTetrominoes.length);
       current = theTetrominoes[random][currentRotation];
@@ -90,15 +107,40 @@ document.addEventListener('DOMContentLoaded', () => {
   // move the tetromino left, unless is at the edge or there is a blockage
   function moveLeft() {
     undraw();
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+    const isAtLeftEdge = current.some((index) => (currentPosition + index) % width === 0);
 
     if (!isAtLeftEdge) currentPosition -=1;
 
-    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+    if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
       currentPosition +=1;
     }
 
     draw();
+  }
+
+  // move the tetromino right, unless is at the edge or there is a blockage
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some((index) => (currentPosition + index) % width === width -1));
+
+    if (!isAtRightEdge) currentPosition += 1
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -=1
+    }
+
+    draw();
+  }
+
+  // rotate the tetromino 
+  function rotate() {
+      undraw();
+      currentRotation ++;
+      if (currentRotation === current.length) { // if the current rotation gets to 4, make it go back to 0
+          currentRotation = 0 
+      }
+      current = theTetrominoes[random][currentRotation];
+      draw();
   }
 
 });
