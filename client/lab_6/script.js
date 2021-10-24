e021de8060d4d9ad5f58491479c973270dfc4df4
reinesmaliksi <rmaliksi@umd.edu>
2021-10-24 19:46:40 -0400

    update 3

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,45 +1,51 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const { visible } = require("chalk");
 
-let cities = [];
+async function windowActions {
+    
+    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-fetch(endpoint)
-    .then((blob => blob.json())
-    .then((data => cities.push(...data));
+    const request = await fetch(endpoint)
+        .then(blob => blob.json())
+        .then(data => cities.push(...data));
 
-function findMatches(wordToMatch, cities) {
-    return cities.filter(place => {
-        //here we need to figure out if the city or state matches was searched
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex)
-    });
+    function findMatches(wordToMatch, cities) {
+        return cities.filter(place => {
+            //here we need to figure out if the city or state matches what was searched
+            const regex = new RegExp(wordToMatch, 'gi');
+            return place.city.match(regex) || place.state.match(regex)
+        });
+    }
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
+    function displayMatches(event) {
+        const matchArray = findMatches(event.target.value, cities);
+        const html = matchArray.map(place => {
+            const regex = new RegExp(event.target.value, 'gi');
+            const cityName = place.city.replace(regex, `<span class='h1'>${this.value}</span>`);
+            const stateName = place.state.replace(regex, `<span class='h1'>${this.value}</span>`);
+            return `
+            <li>
+                <span class="name">${cityName},${stateName}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+            `;
+        }).join('');
+        suggestions.innerHTML = html;
+    }
+
+    const searchInput = document.querySelector('.search');
+    const suggestions = document.querySelector('.suggestions');
+
+    searchInput.addEventListener('change', displayMatches);
+    searchInput.addEventListener('keyup', (evt) => {displayMatches(evt)};
+
+    const cities = await request.json();
 }
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
-
-function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
-    const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span class='h1'>${this.value}</span>`);
-        const stateName = place.state.replace(regex, `<span class='h1'>${this.value}</span>`);
-        return `
-        <li>
-            <span class="name">${cityName},${stateName}</span>
-            <span class="population">${numberWithCommas(place.population)}</span>
-        </li>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
-}
-
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+window.onload = windowActions;
 
 //code from lecture
 
@@ -58,29 +64,37 @@ async function fetchRequest(url) {
     }
 }
 
-function mainThread() {
+async function mainThread() {
     console.log('loaded main script');
     const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+    const inputBox = document.querySelector('#zipcode');
+    const visibleListOfFilteredItems = document.querySelector('.append-box');
 
     /*Actually linked script example*/
     const targetElement = document.querySelector('.click_demo');
     targetElement.addEventListener('click', (event) => runMeOnClickEvent(event));
 
-    const fetchElement = document.querySelector('.fetch');
-    fetchElement.addEventListener('click', async (event) => {
-        const data = await fetchRequest(url);
-        console.log('dataset size from county', data.length);
-        const displaySet = data.filter((item, index) => {
-            const zipcode = '20742';
-            return item.zip === zipcode;
-        });
-        console.long('displaySet contents', displaySet.length);
-    });
+    const data = await fetchRequest(url)
+    console.log('external dataset', data);
 
-    const inputBox = document.querySelector('#zipcode');
     inputBox.addEventListener('input', (event) => {
         console.log(event.target.value);
-    })
+        const filteredList = data.filter((item, index) => {
+            const zipcode = event.target.value;
+            return item.zip === zipcode;
+        });
+        console.table(filteredList);
+        
+        filteredList.forEach((item, index) => {
+            visibleListOfFilteredItems.innerHTML += `<span class='resto-name'>${item.name} <br>`;
+        })
+    });
 }
 
 window.onload = mainThread;
+
+//const fetchElement = document.querySelector('.fetch');
+    //fetchElement.addEventListener('click', async (event) => {
+        //console.log('dataset size from county', data.length);
+        //console.long('displaySet contents', displaySet.length);
+    //});
